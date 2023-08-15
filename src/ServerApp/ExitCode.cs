f5c73c0dbdea6fcebb3b4593e53ae95b2ad89289
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2023-08-15 10:50:18 -0500

    Initial commit

---
+++ src/ServerApp/ExitCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+  internal static class ExitCode
+  {
+    public const int OK = 0;
+    public const int ParseFailed = 1;
+    public const int CertificateError = 2;
+  }
+}
---
+++ src/ServerApp/ProgramOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+  internal class ProgramOptions
+  {
+    public bool AutoAccept = false;
+    public string? CertificateFilename = null;
+    public string? CertificatePassword = null;
+    public string? LogFile = null;
+  }
+}
